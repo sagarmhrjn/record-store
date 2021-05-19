d97810bf7156a3ce0bc5a1570e39f999c9563a0f
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-05-20 02:05:18 +0545

    change in artist migration and route config of jwt authentication and current user record and artist

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'signup/create'
   namespace :api do
     namespace :v1 do
       resources :records
@@ -9,5 +8,12 @@ Rails.application.routes.draw do
   end
 
   root to: "home#index"
+
+  post "refresh", controller: :refresh, action: :create
+  post "signin", controller: :signin, action: :create
+  post "signup", controller: :signup, action: :create
+  delete "signin", controller: :signin, action: :destroy
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2021_05_19_143641) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
-    t.integer "user_id", null: false
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_artists_on_user_id"
