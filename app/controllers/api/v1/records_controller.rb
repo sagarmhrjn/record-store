d3d4b4b70b227b8ee66f9bf6d0c2498c0e8eb6a5
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-05-22 19:47:53 +0545

    fixed project namespace url for saving new instance of record and new artist

--- app/controllers/api/v1/records_controller.rb
+++ app/controllers/api/v1/records_controller.rb
@@ -21,7 +21,7 @@ module Api
         @record = current_user.records.build(record_params)
     
         if @record.save
-          render json: @record, status: :created, location: @record
+          render json: @record, status: :created, location: api_v1_record_url(@record)
         else
           render json: @record.errors, status: :unprocessable_entity
         end
