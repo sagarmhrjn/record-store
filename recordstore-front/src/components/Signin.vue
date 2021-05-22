e66ec8f9172d524c901557b1310431b4b24b6d85
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-05-22 19:56:19 +0545

    header, login, register, artist and records components added

---
+++ recordstore-front/src/components/Signin.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="max-w-sm m-auto my-8">
+    <div class="border p-10 border-grey-light shadow rounded">
+      <h3 class="text-2xl mb-6 text-grey-darkest">Sign In</h3>
+      <form @submit.prevent="signin">
+        <div class="text-red" v-if="error">{{ error }}</div>
+
+        <div class="mb-6">
+          <label for="email" class="label">E-mail Address</label>
+          <input type="email" v-model="email" class="input" id="email" placeholder="someone@example.com">
+        </div>
+        <div class="mb-6">
+          <label for="password" class="label">Password</label>
+          <input type="password" v-model="password" class="input" id="password" placeholder="Password">
+        </div>
+        <button type="submit" class="font-sans font-bold px-4 rounded cursor-pointer no-underline bg-green hover:bg-green-dark block w-full py-4 text-white items-center justify-center">Sign In</button>
+
+        <div class="my-4"><router-link to="/signup" class="link-grey">Sign up</router-link></div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Signin',
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: ''
+    }
+  },
+/**
+ * Called synchronously after the instance is created. 
+ * At this stage, the instance has finished processing the options which means the following have been set up: data observation, computed properties, methods, watch/event callbacks. 
+ * However, the mounting phase has not been started, and the $el property will not be available yet.
+ */
+  created () {
+    this.checkSignedIn()
+  },
+/**
+ * Called after a data change causes the virtual DOM to be re-rendered and patched.
+ * The component's DOM will have been updated when this hook is called, so you can perform
+ * DOM-dependent operations here. However, in most cases you should avoid changing state inside the hook. 
+ * To react to state changes, it's usually better to use a computed property or watcher instead.
+ * */   
+  updated () {
+    this.checkSignedIn()
+  },
+  methods: {
+    signin () {
+      this.$http.plain.post('/signin', { email: this.email, password: this.password })
+        .then(response => this.signinSuccessful(response))
+        .catch(error => this.signinFailed(error))
+    },
+    signinSuccessful (response) {
+      if (!response.data.csrf) {
+        this.signinFailed(response)
+        return
+      }
+      localStorage.csrf = response.data.csrf
+      localStorage.signedIn = true
+      this.error = ''
+      this.$router.replace('/records')
+    },
+    signinFailed (error) {
+      this.error = (error.response && error.response.data && error.response.data.error) || ''
+      delete localStorage.csrf
+      delete localStorage.signedIn
+    },
+    checkSignedIn () {
+      if (localStorage.signedIn) {
+        //  acts like router.push, the only difference is that it navigates without pushing a new history entry, as its name suggests - it replaces the current entry.
+        this.$router.replace('/records')
+      }
+    }
+  }
+}
+</script>
